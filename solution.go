f5821212b6e151620fc9a2e2464292fcb6aee471
Maksim Melnikau <maksim_melnikau@epam.com>
2022-02-11 21:19:41 +0300

    Lab 00 solution-3

--- solution.go
+++ solution.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GetMessage() string {
-	message := emoji.Sprintf("Hello :world_map:! !")
+	message := emoji.Sprintf("Hello :world_map:!")
 	//fmt.Println(message)
 	return message
 }
